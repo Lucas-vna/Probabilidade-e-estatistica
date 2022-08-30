cb15b0a36f0e1af708b88aa488eaf981a1314839
Lucas dos Reis Viana <55816219+Lucas-vna@users.noreply.github.com>
2022-08-30 12:49:30 -0300

    Update Resumos Aula 3 - Gráficos.r

--- Resumos Aula 3 - Gráficos.r
+++ Resumos Aula 3 - Gráficos.r
@@ -1,1 +1,7 @@
+dados1 = read.csv('dados_plot.csv', sep = ",", dec = ",", header = T)
+dados1
 
+#arrumar nome variável
+name(dados1)
+names(dados1) = c('linhas','empresa','rendimento','tamanho', 'vendas', 'palavra', 'fre_palavra')
+head(dados1)
